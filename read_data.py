6f0c4a966f0c240c22548da8c0841233a42ac692
Ashutosh Dwivedi <adwivedi@akamai.com>
2018-04-01 18:47:21 +0530

    added utils to read data using pytorch

--- read_data.py
+++ read_data.py
@@ -10,24 +10,24 @@ def read_data():
     labels = pd.read_csv(data_dir + 'labels.csv')
 
     # only top 20 classes
-    # idx = np.zeros(labels.shape[0], np.bool)
-    # for breed in labels['breed'].value_counts()[:20].index.values:
-    #     idx = idx | (labels['breed'] == breed)
+    idx = np.zeros(labels.shape[0], np.bool)
+    for breed in labels['breed'].value_counts()[:10].index.values:
+        idx = idx | (labels['breed'] == breed)
 
     # labels = labels[idx]
 
     n_classes = len(np.unique(labels['breed']))
     path = data_dir + 'train/{}.jpg'
-    width = 128
-    height = 128
+    width = 32
+    height = 32
 
     images = []
     for filename, _ in labels.values:
         image = cv2.imread(path.format(filename))
-        resized = cv2.resize(image, (width, height)) / 255
+        resized = cv2.resize(image, (width, height))
         images.append(resized)
 
     features = np.array(images, np.float32)
-    features -= features.mean(axis=0)
+    # features -= features.mean(axis=0)
     targets = pd.get_dummies(labels['breed'], sparse=True).values
     return features, targets, n_classes
